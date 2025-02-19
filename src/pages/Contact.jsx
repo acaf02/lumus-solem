53572bda48388cb9d7fae49b5de234c0407f547b
acaf02 <anacarol.alves1@outlook.com>
2025-02-19 20:27:25 -0300

    feat: add new information about contact]

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import Title from "../components/Title";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col gap-16 md:flex-row items-center justify-center min-h-screen  md:px-20 ">
+    <div>
+    <div className="flex flex-col gap-16 md:flex-row items-center justify-center min-h-screen md:px-20 ">
       {/* Texto */}
       <div className="w-1/2 text-center md:text-left">
-        <h2 className="text-4xl font-bold text-gray-800 mb-4">CONTATE-NOS</h2>
-        <form className="flex flex-col bg-custom-yellow pl-8 py-4">
+      <div className="pb-3">
+        <Title text1="CONTATE" text2="-NOS" />
+      </div>
+        <form className="flex flex-col bg-custom-yellow rounded-lg pl-8 py-4">
           <label className="flex flex-col pr-9">
             <span className="pb-1">Nome:</span>
             <input
@@ -24,7 +28,7 @@ const Contact = () => {
             <input
               type="email"
               name="email"
-              placeholder="Digite o seu email"
+              placeholder="Digite o seu email:"
               className="w-full h-12 border border-gray-300 rounded pl-3 bg-gray-100"
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -33,9 +37,9 @@ const Contact = () => {
           <label className="flex flex-col pt-2 pr-9">
             <span className="pb-1">Assunto</span>
             <input
-              type="email"
-              name="email"
-              placeholder="Digite o seu email"
+              type="text"
+              name="text"
+              placeholder="Digite o assunto:"
               onChange={(e) => setEmail(e.target.value)}
               className="w-full h-12 border border-gray-300 rounded pl-3 bg-gray-100"
             />
@@ -44,9 +48,9 @@ const Contact = () => {
           <label className="flex flex-col pt-2 pb-5 pr-9">
             <span className="pb-1">Mensagem</span>
             <textarea
-              type="email"
-              name="email"
-              placeholder="Digite o seu email"
+              type="text"
+              name="text"
+              placeholder="Digite a mensagem:"
               onChange={(e) => setEmail(e.target.value)}
               className="w-full h-12 border border-gray-300 rounded pl-3 h-28 pt-2 bg-gray-100"
             />
@@ -65,6 +69,23 @@ const Contact = () => {
         />
       </div>
     </div>
+    <div className="justify-center md:px-20 pt-10">
+      <div className="flex flex-row gap-4 w-full items-center gap-12">
+        <div className="bg-custom-yellow rounded-lg p-4 w-80 h-48 flex flex-col items-center text-center pt-7 shadow-md">
+          <img src={assets.gmail} className="w-16 h-16" />
+          <p className="pt-9">Email: contato@empresa.com</p>
+        </div>
+        <div className="bg-custom-yellow rounded-lg w-80 h-48 text-center shadow-md flex flex-col items-center pt-9">
+          <img src={assets.whatsapp} className="w-13 h-13"/>
+          <p className="pt-9">Telefone: (11) 99999-9999</p>
+        </div>
+        <div className="bg-custom-yellow rounded-lg p-4 w-80 h-48 text-center shadow-md flex flex-col items-center pt-9">
+          <img src={assets.instagram} className="w-13 h-13" />
+          <p className="pt-9">@empresa</p>
+        </div>
+      </div>
+      </div>
+      </div>
   );
 };
 
