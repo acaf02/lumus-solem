d48153cc2083c1d75b76f409e0d4b73927bc15b2
Ana <122612165+acaf02@users.noreply.github.com>
2025-02-21 14:11:25 -0300

    feat: change the text about the store

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,48 +8,47 @@ import Title from "../components/Title";
 const About = () => {
   return (
     <div className="my-10 flex flex-col text-justify">
-      <div className="justify-center flex pb-5">
-      <Title text1={"SOBRE"} text2={"NÓS"} />
+      {/* Título */}
+      <div className="flex justify-center pb-5">
+        <Title text1="SOBRE" text2="NÓS" />
       </div>
-      <div className="flex flex-row h-[500px]"> 
+
+      <div className="flex flex-row h-[500px]">
         {/* Imagem */}
         <div className="relative flex-1">
-          <img 
-            className="w-full h-full object-cover rounded-bl-lg rounded-tl-lg shadow-lg" 
-            src={assets.about} 
-            alt="Sobre nós" 
+          <img
+            className="w-full h-full object-cover rounded-bl-lg rounded-tl-lg shadow-lg"
+            src={assets.about}
+            alt="Sobre nós"
           />
         </div>
 
         {/* Texto */}
-        <div className="flex-1 flex flex-col justify-center rounded-br-lg rounded-tr-lg bg-custom-yellow pr-7 pl-5 shadow-lg">
+        <div className="flex-1 flex flex-col justify-center rounded-br-lg rounded-tr-lg bg-custom-yellow px-7 shadow-lg">
           <div className="text-gray-800">
             <b>Bem-vindo à Lumus Solem!</b>
-            <p className="pb-4 pt-4">
-              Na Lumus Solem, acreditamos que cada chama carrega consigo um
-              toque de aconchego e harmonia. Nossas velas são cuidadosamente
-              feitas à mão, combinando cera vegetal de alta qualidade e
-              fragrâncias envolventes para proporcionar uma experiência
-              sensorial única e sustentável.
+            <p className="py-4">
+              Lumus Solem foi primeiramente visto (e usado) em <i>Harry Potter e a Pedra Filosofal</i>, pela personagem Hermione Granger para escapar de um perigo em uma grande escuridão. O feitiço em si é usado para criar um feixe de luz muito brilhante, que pode ser mais quente que a luz comum.
             </p>
             <p className="pb-4">
-              Mais do que iluminar ambientes, nossas criações transformam
-              espaços em verdadeiros refúgios de bem-estar, trazendo serenidade,
-              conforto e um toque especial à sua rotina. Cada vela é
-              desenvolvida com amor e dedicação, refletindo nosso compromisso
-              com a qualidade, o design e a sustentabilidade.
+              A ideia do nome surge pela forte ligação da empreendedora com a série e pela idealização de que as velas produzidas possam iluminar cada casa, afastando as sombras e acalentando o ambiente.
+            </p>
+            <p className="pb-4">
+              Na Lumus Solem, acreditamos que cada chama carrega consigo um toque de aconchego e harmonia. Nossas velas são cuidadosamente feitas à mão, combinando cera vegetal de alta qualidade e fragrâncias envolventes para proporcionar uma experiência sensorial única e sustentável.
+            </p>
+            <p className="pb-4">
+              Mais do que iluminar ambientes, nossas criações transformam espaços em verdadeiros refúgios de bem-estar, trazendo serenidade, conforto e um toque especial à sua rotina. Cada vela é desenvolvida com amor e dedicação, refletindo nosso compromisso com a qualidade, o design e a sustentabilidade.
             </p>
             <p>
-              Descubra a magia das velas Lumus Solem e permita-se viver momentos
-              de pura tranquilidade.
+              Descubra a magia das velas Lumus Solem e permita-se viver momentos de pura tranquilidade.
             </p>
           </div>
 
           {/* Botão de contato */}
-          <Link className="cursor-pointer" to={`/contact`}>
-            <div className="flex flex-row text-gray-800 pt-4">
+          <Link className="cursor-pointer" to="/contact">
+            <div className="flex flex-row items-center text-gray-800 pt-4">
               <b className="text-sm">ENTRE EM CONTATO</b>
-              <MdArrowRightAlt className="justify-center w-8 h-5" />
+              <MdArrowRightAlt className="w-8 h-5 ml-2" />
             </div>
           </Link>
         </div>
