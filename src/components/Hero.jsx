36e3e1f4a2ffbee077f76cd73cbfff32d8e2d5d6
acaf02 <anacarol.alves1@outlook.com>
2025-02-22 15:22:08 -0300

    feat: add a image as the background and personalized titles for each page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { assets } from "../assets/assets";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const Hero = () => {
-    return (
-      <div id="content" className="relative w-full h-screen flex justify-center items-center">
-        <div className="hero w-full h-full">
-          {assets.hero ? (
-            <img 
-              
-              alt="Imagem Hero" 
-              className="w-scream h-full object-cover" 
-            />
-          ) : (
-            <p className="text-center text-gray-500">Imagem não carregada</p>
-          )}
-        </div>
-      </div>
-    );
+  const location = useLocation();
+
+  // Define frases para cada rota
+  const heroTexts = {
+    "/": "Bem-vindo à nossa loja de velas aromáticas!",
+    "/shop": "Explore nossa coleção exclusiva de velas.",
+    "/about": "Conheça nossa história e nossos valores.",
+    "/contact": "Entre em contato conosco para mais informações.",
+    "/ourCandles" : "Descubra como nossa velas são feitas e mais!",
+    "/collection": "Descubra as coleções que fazem parte da Lumus Solem!",
+    "/product": "Descubra nossos produtos incríveis!"
   };
 
-export default Hero
+  const currentText = heroTexts[location.pathname] || "Descubra nossos produtos incríveis!";
+
+  return (
+    <div id="content" className="relative w-full h-screen flex justify-center items-center text-center text-white">
+      <div className="hero w-full h-full relative">
+        {/* Imagem de fundo */}
+        {assets.hero ? (
+          <img 
+            src={assets.hero}
+            alt="Imagem Hero" 
+            className="w-full h-full object-cover" 
+          />
+        ) : (
+          <p className="text-gray-500">Imagem não carregada</p>
+        )}
+
+        {/* Camada de overlay cobreado cinza */}
+        <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50"></div>
+
+        {/* Texto sobreposto */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-6 rounded-lg flex flex-col items-center">
+          <h1 className="text-5xl">{currentText}</h1>
+          <FaArrowDownLong className='w-26 h-16 pt-5'/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
