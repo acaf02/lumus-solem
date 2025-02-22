9d4f3edae770e0fe38d17c1fdf1ae79e8ba60c13
acaf02 <anacarol.alves1@outlook.com>
2025-02-22 15:26:25 -0300

    feat: add bestseller importation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navigation from '../components/Navigation'
 import Hero from '../components/Hero'
 import MVV from '../components/MVV'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Navigation />
       <Hero />
+      <BestSeller />
       <MVV />
     </div>
   )
