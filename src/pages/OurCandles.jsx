47544eababbbb5d494b5318b027d394d03375703
acaf02 <anacarol.alves1@outlook.com>
2025-02-22 15:27:06 -0300

    refactor: change the layout of the page and add hero importation

--- src/pages/OurCandles.jsx
+++ src/pages/OurCandles.jsx
@@ -1,86 +1,67 @@
-import React from "react";
-import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import Hero from "../components/Hero";
+import Title from "../components/Title";
 
-const OurCandles = () => {
+export default function AboutPage() {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[5vw]">
+    <div>
+      <Hero />
+      {/* About Section */}
+      <section className="text-center py-16">
       <Title text1="NOSSAS" text2="VELAS" />
+        <p className="text-gray-600 mt-2">Saiba mais sobre como nossas velas são feitas, dicas para uma boa queima e como reutilizar o seu recipiente.</p>
+      </section>
 
-      <div className="flex flex-row rounded shadow-lg">
-        <img src={assets.making} className="w-1/3 h-1/3 rounded-bl-lg rounded-tl-lg bg-custom-yellow" alt="Fabricação das velas" />
-        <div className="flex-1 flex flex-col justify-center rounded-tr-lg rounded-br-lg bg-custom-yellow px-7 ">
-          <div className="text-gray-800">
-            <b>Sobre as Nossas Velas</b>
-            <p className="py-4">
+      {/* Sections */}
+      <div className="px-10 space-y-16">
+        <div className="flex flex-col lg:flex-row items-center gap-28">
+          <img src={assets.making} alt="Sobre as Nossas Velas" className="w-full sm:w-1/2 shadow lg:w-[27vw] rounded-full" />
+          <div className="lg:w-1/2 lg:text-lg">
+            <h3 className="text-2xl font-bold">Sobre as Nossas Velas</h3>
+            <p className="text-gray-600 mt-4">
               Nossas velas são produzidas com essências premium de alta
               qualidade e com a famosa cera de coco, sendo uma opção vegana e
               sustentável.
             </p>
-            <b>Veja alguns benefícios:</b>
-            <p className="pb-4">
-              <b>Queima limpa e prolongada:</b> velas de cera de coco queimam de forma
-              mais lenta e uniforme, durando muito mais tempo.
-            </p>
-            <p className="pb-4">
-              <b>Aroma suave e natural:</b> proporciona um ambiente aconchegante e
-              acolhedor, sem liberar toxinas.
-            </p>
-            <p className="pb-4">
-              <b>Sustentável:</b> produzida de forma ecológica, é biodegradável e não
-              agride o meio ambiente.
-            </p>
+            <b className="text-gray-600"> Veja alguns benefícios:</b>
+            <ul className="text-gray-600 mt-4 list-disc list-inside">
+              <li><b>Queima limpa e prolongada:</b> velas de cera de coco queimam de forma mais lenta e uniforme, durando muito mais tempo.</li>
+              <li><b>Aroma suave e natural:</b> proporciona um ambiente aconchegante e acolhedor, sem liberar toxinas.</li>
+              <li><b>Sustentável:</b> produzida de forma ecológica, é biodegradável e não agride o meio ambiente.</li>
+            </ul>
           </div>
         </div>
-      </div>
 
-      <div className="flex flex-row py-6">
-        <div className="flex-1 flex flex-col justify-center rounded-tl-lg rounded-bl-lg bg-custom-yellow px-7 shadow-lg">
-          <div className="text-gray-800">
-            <b>Dicas para uma boa queima</b>
-            <p>
-              1. Para uma queima uniforme, sempre que acender a vela, deixe a
-              cera da superfície derreter até alcançar as bordas do recipiente,
-              formando uma piscina de cera líquida completa. Não apague a vela
-              antes disso (1h40 a 2h30 de queima).
-            </p>
-            <p>
-              2. Toda vela tem seu limite de queima. Não deixe queimar por mais
-              de 3 horas seguidas.
-            </p>
-            <p>
-              3. Acenda sua vela em superfícies lisas para garantir uma queima
-              uniforme. Utilize a rampa de madeira que a acompanha.
-            </p>
-            <p>
-              4. Sempre que reacender a vela, apare o pavio e o deixe no tamanho
-              ideal de 5mm.
-            </p>
+        <div className="flex flex-col lg:flex-row-reverse items-center gap-28">
+          <img src={assets.about} alt="Dicas para uma boa queima" className="w-full sm:w-1/2 lg:w-[27vw] shadow rounded-lg" />
+          <div className="lg:w-1/2 md:text-1x2 lg:text-lg">
+            <h3 className="text-2xl font-bold">Dicas para uma boa queima</h3>
+            <ul className="text-gray-600 mt-4 list-disc list-inside">
+              <li>Para uma queima uniforme, sempre que acender a vela, deixe a cera derreter até alcançar as bordas do recipiente.</li>
+              <li>Não deixe a vela queimar por mais de 3 horas seguidas.</li>
+              <li>Acenda sua vela em superfícies lisas para garantir uma queima uniforme.</li>
+              <li>Sempre que reacender a vela, apare o pavio e o deixe no tamanho ideal de 5mm.</li>
+            </ul>
           </div>
         </div>
-        <img src={assets.tips} className="w-[40vw] h-full rounded-br-lg rounded-tr-lg shadow-lg border-t border-b border-r" alt="Dicas de queima" />
-      </div>
 
-      <div className="flex flex-row py-6">
-        <div className="flex-1 flex flex-col justify-center rounded-tl-lg rounded-bl-lg bg-custom-yellow px-7 shadow-lg">
-          <div className="text-gray-800">
-            <b>Reutilizando o seu recipiente</b>
-            <p className="py-4">
-              1. Retire o pavio do pote e descarte corretamente o resto de cera
-              que possa ter ficado no recipiente.
-            </p>
-            <p>2. Lave o recipiente.</p>
-
-            <b>Sugestões para reutilização do seu recipiente:</b>
-            <p>- Utilize como decoração para um ambiente.</p>
-            <p>- Guarde pequenos objetos.</p>
-            <p>- Troque seu recipiente por descontos na loja.</p>
+        <div className="flex flex-col lg:flex-row items-center gap-28">
+          <img src={assets.ourcandle} alt="Reutilizando o seu recipiente" className="w-full sm:w-1/2 lg:w-[27vw] shadow rounded-lg" />
+          <div className="lg:w-1/2 lg:text-lg ">
+            <h3 className="text-2xl font-bold">Reutilizando o seu recipiente</h3>
+            <ul className="text-gray-600 mt-4 list-disc list-inside">
+              <li>Retire o pavio do pote e descarte corretamente o resto de cera.</li>
+              <li className="pb-4">Lave o recipiente.</li>
+              <b>Sugestões para reutilização do seu recipiente:</b>
+              <ul className="list-disc list-inside ml-4 p-2">
+                <li>Utilize como decoração para um ambiente.</li>
+                <li>Guarde pequenos objetos.</li>
+                <li>Troque seu recipiente por descontos na loja.</li>
+              </ul>
+            </ul>
           </div>
         </div>
-        <img src={assets.youknow} className="w-1/3 h-1/3 rounded-br-lg rounded-tr-lg shadow-lg" alt="Reutilização do recipiente" />
       </div>
     </div>
   );
-};
-
-export default OurCandles;
+}
