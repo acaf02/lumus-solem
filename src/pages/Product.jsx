69c5c78046aee906a96364400022c347aca12310
acaf02 <anacarol.alves1@outlook.com>
2025-04-27 20:23:28 -0300

    feat: add product list

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Hero from '../components/Hero';
+import { useNavigate } from 'react-router-dom';
+import Title from "../components/Title";
+import ProductCard from '../components/ProductCard';
 
 const Product = () => {
+  const { products } = useContext(ShopContext);
+  const navigate = useNavigate();
+
   return (
     <div>
       <Hero />
-      Product</div>
-  )
-}
 
-export default Product
+      <div className='flex justify-center pt-8'>
+        <Title text2="PRODUTOS" />
+      </div>
+
+      <div className="px-4 sm:px-6 md:px-8 lg:px-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <ProductCard
+            key={product._id}
+            product={product}
+            onClick={() => navigate(`/product/${product._id}`)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
