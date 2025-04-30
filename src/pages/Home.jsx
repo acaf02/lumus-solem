04bb31c50a0e45b730db6fcb43d42733c3fa8c57
acaf02 <anacarol.alves1@outlook.com>
2025-04-29 21:29:09 -0300

    feat: add new section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Navigation from '../components/Navigation'
 import Hero from '../components/Hero'
 import MVV from '../components/MVV'
 import BestSeller from '../components/BestSeller'
+import LatestProduct from '../components/LatestProducts'
 
 const Home = () => {
   return (
     <div>
       <Navigation />
       <Hero />
+      <LatestProduct />
       <BestSeller />
       <MVV />
     </div>
